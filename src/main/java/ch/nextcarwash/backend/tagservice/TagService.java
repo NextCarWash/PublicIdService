e77995b5452dfcd4bef584ba59c5d9cabd500388
Stefan Urech <stefan@surech.ch>
2015-09-17 19:02:07 +0200

    StU: Endlich funktioniert die REST-Schnittstelle. Zumindest ein bisschen.

--- src/main/java/ch/nextcarwash/backend/tagservice/TagService.java
+++ src/main/java/ch/nextcarwash/backend/tagservice/TagService.java
@@ -1,11 +1,14 @@
 package ch.nextcarwash.backend.tagservice;
 
+import ch.nextcarwash.backend.tagservice.model.TagEntity;
 import ch.nextcarwash.backend.tagservice.repo.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Verwaltet die Eigenschaften aka "Tags"
@@ -18,9 +21,22 @@ public class TagService {
     @Autowired
     private TagRepository tagRepository;
 
+    @PostConstruct
+    private void init() {
+        System.out.println("======================");
+        System.out.println(" Ich bin gestartet!!!");
+        System.out.println("======================");
+
+        TagEntity tag = new TagEntity();
+        tag.setPublicId("TG1234");
+        tag.setName("Waschstrasse");
+        tagRepository.save(tag);
+    }
+
     public static void main(String[] args) {
         // Will configure using accounts-server.yml
         System.setProperty("spring.config.name", "tag-service");
+        System.setProperty("spring.data.rest.base-path", "/api");
 
         SpringApplication.run(TagService.class, args);
     }
--- src/main/java/ch/nextcarwash/backend/tagservice/repo/TagRepository.java
+++ src/main/java/ch/nextcarwash/backend/tagservice/repo/TagRepository.java
@@ -1,10 +1,11 @@
 package ch.nextcarwash.backend.tagservice.repo;
 
 import ch.nextcarwash.backend.tagservice.model.TagEntity;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
 /**
  * Datenbank-Zugriff auf die Tags
  */
-public interface TagRepository extends Repository<TagEntity, Long> {
+public interface TagRepository extends CrudRepository<TagEntity, Long> {
 }
